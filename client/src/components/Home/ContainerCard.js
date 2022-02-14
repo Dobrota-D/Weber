13789421ac2cf0a3dc4b989c734f44fe9b394dad
Dobrota <dobrotakevin@yahoo.fr>
2022-02-14 14:21:44 +0100

    исправить css

--- client/src/components/Home/ContainerCard.js
+++ client/src/components/Home/ContainerCard.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Swipe from './Swipe'; 
 
+
 export default function ContainerCard() {
+  
+  const URL = process.env.REACT_APP_BACKEND_URL
+  useEffect(()=> {
+    fetch(`${URL}/questions/?nb=3`)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    })  
+  },[])
+
   return (
     <div className="container-cards">
     <Swipe/>
--- client/src/components/Home/Swipe.js
+++ client/src/components/Home/Swipe.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import Reactw from 'react'
 import Cross from "../../assets/svg/Cross";
 import Heart from "../../assets/svg/Heart";
 import Idk from "../../assets/svg/Idk";
 import TinderCard from "react-tinder-card";
+
 
 export default function Swipe() {
   
@@ -16,7 +17,7 @@ export default function Swipe() {
     
     return (
       <div>
-    <TinderCard className='question-card' onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>Hello, World!</TinderCard>
+      <TinderCard className='question-card' onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>Hello, World!</TinderCard>
       <div className="button-container">
       <div className="dislike-button">
         <Cross/>
