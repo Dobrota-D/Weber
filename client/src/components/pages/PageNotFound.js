e7f34b146afb3363c5946f42cacffa2686f1ad13
Ugo <ugo.prenat@gmail.com>
2022-02-15 12:08:13 +0100

    Η σελίδα δεν βρέθηκε (page not found)

--- client/src/components/pages/PageNotFound.js
+++ client/src/components/pages/PageNotFound.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom'
+
+import '../../styles/pageNotFound.css'
 
 export default function PageNotFound() {
-  return <div className='fullscreen-component'>
-    Page not found
+  const locaction = useLocation()
+  
+  return <div className='fullscreen-component page-not-found-component'>
+    <div className="logo">
+      <h1>Weber</h1>
+      <span className="card"></span>
+      <span className="card"></span>
+    </div>
+    
+    <p>La page <span>'{locaction.pathname.slice(1)}'</span> n'existe pas...</p>
+    <Link to='/'>Revenir à l'accueil</Link>
   </div>;
 }
